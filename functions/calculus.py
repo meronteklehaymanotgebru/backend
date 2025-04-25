f9b38c4ee84075fa210be2418b8966b959a40d3a
Meron T. Gebru <meronteklehaymanotgebru@gmail.com>
2025-04-25 09:34:30 +0300

    Python simple arthmetic operation functions

---
+++ functions/calculus.py
@@ -0,0 +1,34 @@
+def square(number):
+    print(number*number)
+
+def add(x,y):
+    result=x+y
+    return result
+
+def multiply(w,u):
+    result=x*y
+    return result
+
+
+def divide(x,y):
+    result=x/y
+    return result
+def substract(x,y):
+    result=x-y
+    return result
+
+def exponent(x,y):
+    result=x**y
+    return result
+
+def modulus(x,y):
+    result=x%y
+    return result
+
+def substract(x,y):
+    result=x%y
+    return result
+
+
+
+    
---
+++ functions/hello.py
@@ -0,0 +1,11 @@
+def hello():
+    print("Hello AkiraChix")
+
+# -m for modules while running it starts reading from the top
+# python3 -m file name  this is called feeding the code to pour interpreter
+# Importing first to interpreter, check the path then 'from module import function'
+# From hello import hello// Either no output or an error
+# Then you call the function in interpreter
+# if we were in functions directory we would say from functions import hello
+def say_hello(name):
+    print(f"hello {name}")
